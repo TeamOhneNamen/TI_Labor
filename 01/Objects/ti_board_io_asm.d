d5dac55c4c21b992b314d154ca8eee705f0c21f2
th0rben <opi70@web.de>
2017-05-10 12:58:27 +0200

    beth

---
+++ 01/Objects/ti_board_io_asm.d
@@ -0,0 +1,1 @@
+.\objects\ti_board_io_asm.o: TI_BRD_LIB\TI-C_Board\TI_Board_IO_asm.s
--- 01/main.s
+++ 01/main.s
@@ -1,9 +1,8 @@
 ;******************** (C) COPYRIGHT HAW-Hamburg ********************************
 ;* File Name          : main.s
-;* Author             : Thorben Schomacker
-;* Author             : Ferdinand Trendelenburg	
+;* Author             : Alfred Lohmann
 ;* Version            : V1.0
-;* Date               : 18.04.2017
+;* Date               : 15.07.2012
 ;* Description        : This is a simple main.
 ;					  : The output is send to UART 1. Open Serial Window when 
 ;					  : when debugging. Select UART #1 in Serial Window selection.
@@ -12,36 +11,23 @@
 ;
 ;*******************************************************************************
 
-	EXTERN Init_TI_Board		; Initialize the serial line
-	EXTERN ADC3_CH7_DMA_Config  ; Initialize the ADC
-	;EXTERN	initHW				; Init Timer
-	EXTERN	puts				; C output function
-	EXTERN	TFT_puts			; TFT output function
-	EXTERN  TFT_cls				; TFT clear function
-	EXTERN  TFT_gotoxy      	; TFT goto x y function  
-	EXTERN  Delay				; Delay (ms) function
-	EXTERN GPIO_G_SET			; Set output-LEDs
-	EXTERN GPIO_G_CLR			; Clear output-LEDs
-	EXTERN GPIO_G_PIN			; Output-LEDs status
-	EXTERN GPIO_E_PIN			; Button status
-	EXTERN ADC3_DR				; ADC Value (ADC3_CH7_DMA_Config has to be called before)
-
-;********************************************
-; Data section, aligned on 4-byte boundery
-;********************************************
+	IMPORT 	Init_TI_Board		; Initialize the serial line
+	;IMPORT	initHW				; Init Timer
+	IMPORT	puts				; C output function
+	IMPORT	TFT_puts			; TFT output function
 	
-	AREA MyData, DATA, align = 4
-	GLOBAL MyData, MeinNumFeld, MeinHaWoFeld, MeinTextFeld, MeinByteFeld, MeinBlock
-		
-MeinNumFeld 	DCD 	0x33, 2_01111110, -57, 66, 0x70000000, 0x80000000
-MeinHaWoFeld 	DCW 	0x1234, 0x5678, 0x9abc, 0xdef0
-MeinTextFeld 	DCB 		"ABab0123",0
-MeinByteFeld 	DCB 0xef, 0xdc, 0xba, 0x98
 
-						ALIGN 4 ; empfehlenswert für hohe Performance, wenn in MeinBlock
-MeinBlock 		SPACE 0x20
-	
-text	DCB	"Hallo TI-Labor.\n\r",0
+;**************************************************
+; Data section, aligned on 16-byte boundery
+;**************************************************
+		AREA MyData, DATA, align = 4
+DataList 	DCD 	35, -1, 13, -4096, 511, 101, -3, -5, 0, 65
+DataListEnd DCD 	0
+
+		GLOBAL DataList
+		GLOBAL DataListEnd
+
+
 
 ;********************************************
 ; Code section, aligned on 8-byte boundery
@@ -56,6 +42,21 @@ text	DCB	"Hallo TI-Labor.\n\r",0
 	
 main	PROC
 
+;Getauscht <- Ja
+;while Getauscht == Ja do
+;Getauscht <- Nein
+;Zeiger auf den ersten Wert setzen
+;while Zeiger zeigt nicht auf den letzten Wert do
+;if aktueller Wert > folgender Wert then
+;Tabelleneinträge tauschen
+;Getauscht <- Ja
+;end if
+;Zeiger auf den folgenden Eintrag setzen
+;end while
+;end while
+		
+		
+		
 		BL	Init_TI_Board	; Initialize the serial line to TTY
 							; for compatability to out TI-C-Board
 		;BL	initHW			; Timer init
@@ -65,8 +66,7 @@ main	PROC
 		
 		LDR r0,=text
 		BL	TFT_puts		; call TFT output method
-		
-		
+				
 forever	b	forever		; nowhere to retun if main ends		
 		ENDP
 	
